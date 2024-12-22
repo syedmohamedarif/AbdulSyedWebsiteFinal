3b1d9f46362ec74f706da82136d9a38672f2adbe
syedmohamedarif <158511819+syedmohamedarif@users.noreply.github.com>
2024-12-22 08:47:35 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtZaGoXOHBk4homlAdXIDrS6om3T8F0dw",
-  authDomain: "appointmentdata-c72d6.firebaseapp.com",
-  projectId: "appointmentdata-c72d6",
-  storageBucket: "appointmentdata-c72d6.firebasestorage.app",
-  messagingSenderId: "162220937733",
-  appId: "1:162220937733:web:98f83774e6ee92a277826c",
-  measurementId: "G-MR2GDLJFQ8"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 // Initialize Firebase
